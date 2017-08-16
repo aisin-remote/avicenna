e98b721ae64f0ef2f00da7975ceb4558de72255a
Yudo Maryanto <yudo.maryanto@aiia.co.id>
2017-08-16 16:00:23 +0700

    dev-1.0 : pis ajax show table mutation

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,15 @@ Route::group(['middleware' => 'auth'], function () {
 
 
 	Route::get('/home', 'HomeController@index')->name('home');
+
+	//dev-1.0, 20170816 by yudo, modul pis
 	Route::get('/pis', 'PisController@index')->name('pis');
-	Route::get('/pis/pis_transaction', 'PisController@getPisTransaction');
-	Route::get('/getAjaxImage/{image}', 'PisController@getAjaxImage');
+	Route::get('/pis/getAjaxMutation', 'PisController@getPisTransaction');
+	Route::get('/pis/getAjaxImage/{image}', 'PisController@getAjaxImage');
+	
+
+	Route::post('/pis/insertMutation', 'PisController@insertMutation');
+	//end of modul pis
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
