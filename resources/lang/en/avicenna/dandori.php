919a72b2ba8bbd1608787345186cf772cd722085
AIIA\Alliq <alliq@aiia.co.id>
2017-10-04 10:10:57 +0700

    dev-1.0: Change route

---
+++ resources/lang/en/avicenna/dandori.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'default_title'     => 'Dandori Model',
+    'default_desc'    => 'Change Model',
+    'dandori_entry_box'     => 'Dandori Model', 
+    'label_1'        => 'Choose Line Name',
+    'label_2'        => ' - Now Condition Information -',
+    'label_line'           => 'Line',
+    'label_model'           => 'Model',
+    'label_qty'           => 'Quantity',
+    'save_text'     => 'Save Data',
+    'cancel_text'     => 'Cancel',
+    'lbl_choose_model' => 'Choose New Model',
+    'modal_title_txt'   => 'Confirmation',
+    'modal_body_txt' => 'Do you really want to Save changes ?',
+
+    // '' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    ];
