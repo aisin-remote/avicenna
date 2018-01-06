35fa283220a7571a6b20787f6d3d776ad1e5b616
AIIA\Alliq <alliq@aiia.co.id>
2018-01-06 15:03:19 +0700

    dev-1.0: WIP dandori (modal sandori OK)

--- resources/views/vendor/adminlte/dandori/MakeDandori.blade.php
+++ resources/views/vendor/adminlte/dandori/MakeDandori.blade.php
@@ -2,63 +2,82 @@
 
 @section('content')
 <div class="container">
-    @lang('avicenna/dandori.default_title')
-
-    <div class="row">
-        <div class="col-md-9">
-            <table class="table">
-                <tr>
-                    @foreach($models as $model)
-                    <td> <button class="btn <?php if($running_model->back_number==$model->back_number) echo 'btn-primary '; else echo 'btn-default '; ?>btn-model" id="{{$model->back_number}}" onclick="SetClass(this.id)">{{$model->back_number}}</button></td>
-                    @endforeach
-                </tr>
-            </table>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="panel panel-default">
+      <div class="panel-heading"><b>DANDORI MODEL</b>
+          <a href="javascript:openModal();"  class="btn btn-default pull-right"><i class="icon fa fa-bars"></i></a>
+          <div class="clearfix"></div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-9">
-            <div class="panel panel-default">
-              <div class="panel-heading"><b>GAMBAR MODEL</b></div>
-              <div class="panel-body">
-              </div>
+        <div class="panel-body">
+        <div class="row">
+          <div class="col-md-10">
+            <span class="border border-default">
+              <br/><br/><br/>
+            </span>
           </div>
-      </div>
-      <div class="col-md-3">
-        <div class="panel panel-default">
-              <div class="panel-heading"><b>INFORMASI</b></div>
-              <div class="panel-body">
-               <div class="form-group">
-               <center>
-                   <label  id="running-model" class="control-label" style="font-size:25pt;">{{$running_model->back_number}}</label>
-                   <br/>
-                   <label id="running-model-qty" class="control-label" style="font-size:25pt;">{{$running_model->quantity}}</label>
-                </center>
-               </div>
-              </div>
+          <div class="col-md-2">
+            <br/><br/><br/>
           </div>
-      </div>
-  </div>
-
-{{-- Modal Loading --}}
-<div class="modal fade" id="myModal" role="dialog">
-  <div class="modal-dialog" style="width:350px;">
-    <div class="modal-content">
-      <div class="modal-body">
-      <center>
-      <p><b>Pilih Dandori</b></p>
-        <button class="btn btn-default" id="btn-dandori-start" onclick="Simpan(this.id)">Dandori Awal Shift</button>
-        <button class="btn btn-default" id="btn-dandori-change" onclick="Simpan(this.id)">Dandori di tengah</button>
-        <br/><br/>
-        <div id="loading" hidden>
-            <img src="{{asset('/images/loading.gif')}}"/>
-            <br/>Mohon tunggu
         </div>
-        </center>
+        </div>
       </div>
     </div>
-
+    <div class="col-md-1">
+    </div>
   </div>
-</div>
+
+  {{-- Modal Loading --}}
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog" style="width:800px;">
+      <div class="modal-content">
+        <div class="modal-body">
+          <center><h3>MENU DANDORI</h3></center>
+          <hr/>
+          <center>
+            <?php $i=0?>
+            @foreach($models as $model)
+            <button class="btn btn-default btn-model" id="{{$model->back_number}}" style="margin-right:10px;width:150px;" onclick="ShowButtonControl(this.id)">
+              <h2>{{$model->back_number}}</h2>
+            </button>
+            <?php 
+            $i++;
+            if($i==4){
+              echo"<br/><br/>";
+              $i=0;
+            }
+            ?>
+            @endforeach
+          </center>
+          <hr/>          
+          <div class="row">
+            <div class="col-md-6">
+              <h4><b>Jumlah NG Sebelum Dandori</b></h4>
+              <input class="keyboard form-control input-lg" id="input-ng" type="text" placeholder="Jumlah NG Sebelum Dandori">
+            </div>
+            <div class="col-md-6">
+              <h4><b>Jumlah Seteuchi Sebelum Dandori</b></h4>
+              <input class="keyboard form-control input-lg" id="input-seteuchi" type="text" placeholder="Jumlah Seteuchi Sebelum Dandori">
+            </div>
+          </div>
+          <hr/>
+          <div class="row" id="control-button" hidden>
+            <div class="col-md-6">
+              <center>
+                <button class="btn btn-warning"  style="width:350px;" id="start" onclick="this.id"><h4>Awal Shift</h4></button>
+              </center>
+            </div>
+            <div class="col-md-6">
+              <center>
+                <button class="btn btn-success" style="width:350px;" id="change" onclick="this.id"><h4>Tengah Shift</h4></button>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
 </div>
 
@@ -66,82 +85,153 @@
 
 @section('scripts')
 @parent
-
+<link href="{{ asset ('/plugins/jqbtk/jqbtk.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset ('css/all.css') }}" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="{{asset('/plugins/jqbtk/jqbtk.min.js')}}"></script>
 <script type="text/javascript">
-var qty=0;
-var is_start=false;
-var back_number='';
+  var qty=0;
+  var is_start=false;
+  var back_number='';
+  var qty_ng=0;
+  var qty_seteuchi=0;
 
-    $(document).ready(function(){
-       setInterval(function(){
-        GetQuantity();
-       },3000);
-    });
+    // $(document).ready(function(){
+    //    setInterval(function(){
+    //     GetQuantity();
+    //    },3000);
+    // });
+$('#input-seteuchi').keyboard({type:'numpad'});;
+$('#input-ng').keyboard({type:'numpad'});;
+function ShowButtonControl(id){
+  $('#control-button').show();
+  $('.btn-model').removeClass('btn-primary');
+  $('#'+id).addClass('btn-primary');
+  back_number=id;
+}
 
-    function Simpan(id){
-        $('#loading').show();
-        $('#btn-dandori-start').attr('disabled','disabled');
-        $('#btn-dandori-change').attr('disabled','disabled');
+function Simpan(id){
+  // $('#loading').show();
+  // $('#btn-dandori-start').attr('disabled','disabled');
+  // $('#btn-dandori-change').attr('disabled','disabled');
+  if(id=='start'){
+      is_start=true;
+  }else{
+      is_start=false;
+  }
+  if($('#input-ng').val().length >0){
+    qty_ng=$('#input-ng').val();
+  }
+  if($('#input-seteuchi').val().length >0){
+    qty_seteuchi=$('#input-seteuchi').val();
+  }
+  $.ajax({
+    url: "{{url('/dandori/make')}}",
+    method :'POST',
+    data: {
+      back_number:back_number,
+      _token: "{{csrf_token()}}",
+      is_start:is_start,
+      line_number:'{{$line_number}}',
+      qty_seteuchi:qty_seteuchi,
+      qty_ng:qty_ng
+    },
+    success: function(result){
+      $('.btn-model').removeClass('btn-primary');
+      $('.btn-model').addClass('btn-default');
+      $('#'+back_number).removeClass('btn-default');
+      $('#'+back_number).addClass('btn-primary');
+      $('#running-model').html(back_number);
 
-        if(id=='btn-dandori-start'){
-            is_start=true;
-        }else{
-            is_start=false;
-        }
-        $.ajax({
-          url: "{{url('/dandori/make')}}",
-          method :'POST',
-          data: {
-            back_number:back_number,
-            _token: "{{csrf_token()}}",
-            is_start:is_start,
-            line_number:'{{$line_number}}'
-          },
-          success: function(result){
-            $('.btn-model').removeClass('btn-primary');
-            $('.btn-model').addClass('btn-default');
-            $('#'+back_number).removeClass('btn-default');
-            $('#'+back_number).addClass('btn-primary');
-            $('#running-model').html(back_number);
+      $('#loading').hide();
+      $('#btn-dandori-start').removeAttr('disabled');
+      $('#btn-dandori-change').removeAttr('disabled');
 
-            $('#loading').hide();
-            $('#btn-dandori-start').removeAttr('disabled');
-            $('#btn-dandori-change').removeAttr('disabled');
-
-            closeModal();
-          }
-        });
+      closeModal();
     }
+  });
 
-    function SetClass (id) {
-        back_number=id;
-        openModal();
-    }
+        // if(id=='btn-dandori-start'){
+        //     is_start=true;
+        // }else{
+        //     is_start=false;
+        // }
+        // $.ajax({
+        //   url: "{{url('/dandori/make')}}",
+        //   method :'POST',
+        //   data: {
+        //     back_number:back_number,
+        //     _token: "{{csrf_token()}}",
+        //     is_start:is_start,
+        //     line_number:'{{$line_number}}'
+        //   },
+        //   success: function(result){
+        //     $('.btn-model').removeClass('btn-primary');
+        //     $('.btn-model').addClass('btn-default');
+        //     $('#'+back_number).removeClass('btn-default');
+        //     $('#'+back_number).addClass('btn-primary');
+        //     $('#running-model').html(back_number);
 
-    function GetQuantity(){
-        $.ajax({
-          url: "{{url('/dandori/quantity')}}",
-          method :'GET',
-          data: {
-            line_number:'{{$line_number}}',
-          },
-          success: function(result){
-            qty=result;
-           $('#running-model-qty').html(result);
-          }
-        });
+        //     $('#loading').hide();
+        //     $('#btn-dandori-start').removeAttr('disabled');
+        //     $('#btn-dandori-change').removeAttr('disabled');
+
+        //     closeModal();
+        //   }
+        // });
+}
+
+function SetClass (id) {
+  back_number=id;
+  openModal();
+}
+
+function GetQuantity(){
+  $.ajax({
+    url: "{{url('/dandori/quantity')}}",
+    method :'GET',
+    data: {
+      line_number:'{{$line_number}}',
+    },
+    success: function(result){
+      qty=result;
+      $('#running-model-qty').html(result);
     }
-    function openModal(){
-     $('#myModal').modal({
-      // backdrop: 'static',
+  });
+}
+function openModal(){
+ $('#myModal').modal({
+      backdrop: 'static',
       // keyboard: true
-      });
-    }
+    });
+}
 
-    function closeModal(){
-      $("#myModal").modal('hide');
-    }
+function closeModal(){
+  $("#myModal").modal('hide');
+}
 
+$('#myModal').on('hidden.bs.modal', function () {
+  $('#control-button').hide();
+  $('.btn-model').removeClass('btn-primary');
+})
 </script>
-
+<style>
+/*.btn-default.active.focus, .btn-default.active:focus, .btn-default.active:hover, .btn-default:active.focus, .btn-default:active:focus, .btn-default:hover, .open>.btn-default.dropdown-toggle.focus, .open>.btn-default.dropdown-toggle:focus, .open>.btn-default.dropdown-toggle:hover {
+    color: #fff;
+    background-color: #1f648b;
+    border-color: #133d55;
+  }*/
+  .btn-primary{
+    color: #fff;
+   /* background-color: #f9f9f9;
+    border-color: #133d55;*/
+  }
+  .panel-heading h3 {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-height: normal;
+    width: 75%;
+    padding-top: 8px;
+  }
+</style>
 @endsection
