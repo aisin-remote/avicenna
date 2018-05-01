da40e8f27560e2481f1e92b9424f91372d181066
AIIA\Alliq <alliq@aiia.co.id>
2018-05-01 10:47:16 +0700

    dev-1.0: Add Register Address Column to avi_machinig

---
+++ database/migrations/2018_05_01_104214_add_address_to_avi_machining_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAddressToAviMachiningTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('avi_machining', function (Blueprint $table) {
+            // dev-1.0, Alliq 20180501
+            $table->string('reg_address','6')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('avi_machining', function (Blueprint $table) {
+            // dev-1.0, Alliq, 20180501
+            $table->dropColumn('reg_address');
+        });   
+    }
+}
