179958e1abe220d7accc56b39b4a678f9bee4659
Handika <handika@aiia.co.id>
2018-08-23 10:21:24 +0700

    dev-1.1.0 : update migration avi trace delivery karena typo :D

--- database/migrations/2018_07_26_095952_add_customer_column_on_avi_trace_delivery.php
+++ database/migrations/2018_07_26_095952_add_customer_column_on_avi_trace_delivery.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCustomerColumnOnAviTraceDeliverty extends Migration
+class AddCustomerColumnOnAviTraceDelivery extends Migration
 {
     /**
      * Run the migrations.
