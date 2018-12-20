67688283a5ea44ef1f0744cd05f4bf8184f75a76
Handika <handika@aiia.co.id>
2018-12-20 09:04:39 +0700

    dev-1.1.0 : model avi andon detail

---
+++ app/Models/Avicenna/avi_andon_detail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Avicenna;
+
+use Illuminate\Database\Eloquent\Model;
+
+class avi_andon_detail extends Model
+{
+    
+    protected $table = 'avi_andon_details';
+
+    protected $fillable =[
+    	'back_no',
+    	'line',
+    	'actual_qty_reg_address',
+    	'value_reg',
+    	''
+    ];
+
+}
