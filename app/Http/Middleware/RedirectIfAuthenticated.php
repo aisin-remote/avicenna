eb6dc3823c528dd2a182b1a765aa2779f2b95b5f
Handika <handika@aiia.co.id>
2019-07-10 09:50:03 +0700

    dev-1.0.0 : #ADD route redirect dc, ma and as

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -29,6 +29,21 @@ class RedirectIfAuthenticated
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
 
             }
+            elseif (Auth::user()->hasRole('trace_casting_dcaa01')) {
+                $role = Role::findByName('trace_casting_dcaa01');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_casting_dcaa02')) {
+                $role = Role::findByName('trace_casting_dcaa02');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_casting_dcaa03')) {
+                $role = Role::findByName('trace_casting_dcaa03');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
             elseif (Auth::user()->hasRole('trace_casting_dcaa04')) {
                 $role = Role::findByName('trace_casting_dcaa04');
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
@@ -39,30 +54,80 @@ class RedirectIfAuthenticated
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
 
             }
+            elseif (Auth::user()->hasRole('trace_casting_dcaa06')) {
+                $role = Role::findByName('trace_casting_dcaa06');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_casting_dcaa07')) {
+                $role = Role::findByName('trace_casting_dcaa07');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
             elseif (Auth::user()->hasRole('trace_casting_dcaa08')) {
                 $role = Role::findByName('trace_casting_dcaa08');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_machining_ma001')) {
+                $role = Role::findByName('trace_machining_ma001');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_machining_ma002')) {
+                $role = Role::findByName('trace_machining_ma002');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_machining_ma003')) {
+                $role = Role::findByName('trace_machining_ma003');
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
 
             }
             elseif (Auth::user()->hasRole('trace_machining_ma006')) {
                 $role = Role::findByName('trace_machining_ma006');
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
-            
+
             }
             elseif (Auth::user()->hasRole('trace_machining_ma007')) {
                 $role = Role::findByName('trace_machining_ma007');
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
-            
+
+            }
+            elseif (Auth::user()->hasRole('trace_machining_ma008')) {
+                $role = Role::findByName('trace_machining_ma008');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_assembling_as001')) {
+                $role = Role::findByName('trace_assembling_as001');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_assembling_as002')) {
+                $role = Role::findByName('trace_assembling_as002');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_assembling_as003')) {
+                $role = Role::findByName('trace_assembling_as003');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
+            }
+            elseif (Auth::user()->hasRole('trace_assembling_as004')) {
+                $role = Role::findByName('trace_assembling_as004');
+                return redirect($role->route_redirect ? $role->route_redirect : 'home');
+
             }
             elseif (Auth::user()->hasRole('trace_delivery')) {
                 $role = Role::findByName('trace_delivery');
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
-            
+
             }
             elseif (Auth::user()->hasRole('trace_delivery_ng')) {
                 $role = Role::findByName('trace_delivery_ng');
                 return redirect($role->route_redirect ? $role->route_redirect : 'home');
-            
+
             }
             else {
                 return redirect('home');
