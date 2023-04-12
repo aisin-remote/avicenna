6af61ea3efa46908ef4363f1f1cc6b1878fb2190
AIIA\fabian <fabian@aiia.co.id>
2023-04-12 07:21:09 +0630

    disable API

--- app/Http/Controllers/Avicenna/Api/ApiController.php
+++ app/Http/Controllers/Avicenna/Api/ApiController.php
@@ -14,43 +14,23 @@ class ApiController extends Controller
 {
 
         // get quantity NG
-        $ngQty = DB::table('avi_trace_ngs')
-        ->where('line', $line)
-        ->whereBetween('created_at', [$start_time .'%', $end_time .'%'])
-        ->count();
-
-        // delete temporary
-        // $maQty = DB::table('avi_trace_machining')
-        // ->select(DB::raw('SUBSTRING(code, 3, 2) AS diesid, COUNT(id) AS Jumlah'))
-        // ->groupBy('diesid')
-        // ->whereBetween('created_at', [date('Y-m-d H:00:00'), date('Y-m-d H:59:59')])
-        // ->get();
-
-        // $asQty = DB::table('avi_trace_assemblings')
-        // ->select(DB::raw('SUBSTRING(code, 3, 2) AS diesid, COUNT(id) AS Jumlah'))
-        // ->groupBy('diesid')
-        // ->whereBetween('created_at', [date('Y-m-d H:00:00'), date('Y-m-d H:59:59')])
-        // ->get();
-
-        // $dcQty = DB::table('avi_trace_casting')
-        // ->select(DB::raw('SUBSTRING(code, 3, 2) AS diesid, COUNT(id) AS Jumlah'))
-        // ->groupBy('diesid')
+        // $ngQty = DB::table('avi_trace_ngs')
         // ->where('line', $line)
-        // ->whereBetween('created_at', [date('Y-m-d H:00:00'), date('Y-m-d H:59:59')])
-        // ->get();
+        // ->whereBetween('created_at', [$start_time .'%', $end_time .'%'])
+        // ->count();
 
-        // get quantity DC
-        $dcQty = DB::table('avi_trace_casting')
-        ->where('line', $line)
-        ->whereBetween('created_at', [$start_time .'%', $end_time .'%'])
-        ->count();
+        // // get quantity DC
+        // $dcQty = DB::table('avi_trace_casting')
+        // ->where('line', $line)
+        // ->whereBetween('created_at', [$start_time .'%', $end_time .'%'])
+        // ->count();
 
         // $hour = date('H');
         
         return response()->json([
             'line' => $line,
-            'ngQty' => $ngQty,
-            'dcQty' => $dcQty,
+            'ngQty' => 0,
+            'dcQty' => 0,
         ]);
     }
 
