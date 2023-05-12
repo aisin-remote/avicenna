4545257a64a07e64f615bbbeee70eaef29e68994
Handika <handika@aiia.co.id>
2023-05-13 05:47:50 +0700

    dev-1.0.0 : coba sleep 10 second

--- app/Console/Commands/EmailDashboard.php
+++ app/Console/Commands/EmailDashboard.php
@@ -284,6 +284,7 @@ class EmailDashboard extends Command
             ));
             $response = curl_exec($curl);
             curl_close($curl);
+            sleep(10);
             echo $response;
 
         }
@@ -314,19 +315,26 @@ class EmailDashboard extends Command
 
         // WA message
         foreach ($users as $user) {
-            $param = 0;
-            while ($param < 1) {
-                $firstVal = DB::connection('mysql2')->table('tw_message')->first();
 
-                if (!$firstVal) {
-                    DB::connection('mysql2')->table('tw_message')->insert([
-                        'nowa' => $user->phone_number,
-                        'pesan' => sprintf("```---- ``` *REAL TIME ALERT* ``` ----%cID       : ALERT-$id %cLINE     : ``` *$line* ``` %cSTATUS   : $textstatus %cSOLVETIME: $time Minutes %c-------------------------``` ", 10, 10, 10, 10, 10)
-                    ]);
-
-                    $param++;
-                }
-            }
+            $token = "v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1";
+            $message = sprintf("```---- ``` *REAL TIME ALERT* ``` ----%cID       : ALERT-$id %cLINE     : ``` *$line* ``` %cSTATUS   : $textstatus %cSOLVETIME: $time Minutes %c-------------------------``` ", 10, 10, 10, 10, 10);
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$user->phone_number.'&message='.$message,
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            sleep(10);
+            echo $response;
+            
         }
     }
 
