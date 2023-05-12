bd4b6fe7cd7790301b9580720fee96af4aa499e8
Handika <handika@aiia.co.id>
2023-05-13 01:35:48 +0700

    dev-1.0.0 : implement wa notif

--- app/Console/Commands/EmailDashboard.php
+++ app/Console/Commands/EmailDashboard.php
@@ -267,19 +267,25 @@ class EmailDashboard extends Command
         $errortime = date('H:i:s', strtotime($error));
         // WA message
         foreach ($users as $user) {
-            $param = 0;
-            while ($param < 1) {
-                $firstVal = DB::connection('mysql2')->table('tw_message')->first();
 
-                if (!$firstVal) {
-                    DB::connection('mysql2')->table('tw_message')->insert([
-                        'nowa' => $user->phone_number,
-                        'pesan' => sprintf("```---- ``` *REAL TIME ALERT* ``` ----%cID          : ALERT-$id %cTGL       : $errordate %cJAM     : $errortime %cLINE     : ``` *$line* ``` %cSTATUS   : $textstatus %cDOWNTIME : $time Minutes %c-------------------------``` ", 10, 10, 10, 10, 10, 10, 10, 10)
-                    ]);
+            $token = "v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1";
+            $message = sprintf("```---- ``` *REAL TIME ALERT* ``` ----%cID          : ALERT-$id %cTGL       : $errordate %cJAM     : $errortime %cLINE     : ``` *$line* ``` %cSTATUS   : $textstatus %cDOWNTIME : $time Minutes %c-------------------------``` ", 10, 10, 10, 10, 10, 10, 10, 10);
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$user->phone_number.'&message='.$message,
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
 
-                    $param++;
-                }
-            }
         }
 
         // end
