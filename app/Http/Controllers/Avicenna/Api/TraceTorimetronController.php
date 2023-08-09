d26c2ecbeb9552b74252ad27c0cf8ad96e9f2f11
Handika <handika@aiia.co.id>
2023-08-09 14:28:26 +0700

    dev-1.1.0 : wa torimetron

--- app/Http/Controllers/Avicenna/Api/TraceTorimetronController.php
+++ app/Http/Controllers/Avicenna/Api/TraceTorimetronController.php
@@ -70,21 +70,29 @@ class TraceTorimetronController extends Controller
                 $users = User::where('torimetron_notif', User::RECEIVE_TORIMETRON_NOTIF)->get();
 
                 foreach ($users as $user) {
-                    $param = 0;
-                    while ($param < 1) {
                         $firstVal = DB::connection('mysql2')->table('tw_message')->first();
                         $errorDate = date('Y-m-d', strtotime($dataToSave['datetime_machine']));
                         $errorTime = date('H:i:s', strtotime($dataToSave['datetime_machine']));
                         $productCode = $dataToSave['product_code'];
-                        if (!$firstVal) {
-                            DB::connection('mysql2')->table('tw_message')->insert([
-                                'nowa' => $user->phone_number,
-                                'pesan' => sprintf("```---- TORIMETRON NOTIF ----%cTGL      : $errorDate %cJAM      : $errorTime %cPART     : $productCode %cSTATUS   : NG PART %c------------------------``` ", 13, 13, 13, 13, 13, 13)
-                            ]);
 
-                            $param++;
-                        }
-                    }
+                        $token = "v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1";
+                        $message = sprintf("```---- TORIMETRON NOTIF ----%cTGL      : $errorDate %cJAM      : $errorTime %cPART     : $productCode %cSTATUS   : NG PART %c------------------------``` ", 13, 13, 13, 13, 13, 13);
+                        $curl = curl_init();
+                        curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$user->phone_number.'&message='.$message,
+                        ));
+                        $response = curl_exec($curl);
+                        curl_close($curl);
+                        sleep(10);
+                        echo $response;
                 }
             }
         }
