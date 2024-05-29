a1cb0d3aa9135ed3cd65aa238b5eac53a575da84
Handika <handika@aiia.co.id>
2024-05-29 15:19:10 +0700

    dev-1.0.0 : add new code pis

--- app/Http/Controllers/PisController.php
+++ app/Http/Controllers/PisController.php
@@ -45,30 +45,29 @@ class PisController extends Controller
         // dev-1.0, Ferry, 20170926, Normalisasi string barcode
         $image  = strlen($image) == 208 ? substr($image, 53, 15) : $image;  // dev-1.0, Handika, 20171020, Cust SIM change numb substr
         $image  = strlen($image) == 220 ? substr($image, 158, 10) : $image;  // hotfix-1.0.1, Handika, 20180827, Cust TTI change numb substr
+        $image  = strlen($image) == 20 ? substr($image, 7, 10) : $image;  // hotfix-1.0.1, Handika
 
         if (strlen($image) > 100 && strlen($image) < 200)
             $image = explode('|', $image)[3];
 
-        $image  = str_replace("-","", $image);
+        $image  = str_replace("-", "", $image);
         $image  = strlen($image) == 14 ? substr($image, 0, 10) : $image;
         $image  = strlen($image) == 12 ? (substr($image, -2) == "00" ? substr($image, 0, 10) : $image) : $image;
         $image  = strlen($image) == 13 ? (substr($image, 11, 1) == " " ? substr($image, 0, 10) : substr($image, 0, 12)) : $image; // hotfix-1.0.2, Handika, 20180827, validasi 13 karakter untuk pis packing
 
-        $path_suffix = '-'.$type.'-'.$dock.'.JPG';
+        $path_suffix = '-' . $type . '-' . $dock . '.JPG';
 
-        $part_pis   = avi_part_pis::whereRaw('REPLACE(part_number_customer, "-", "") LIKE "%'.$image.'%"')
-                            ->where('part_kind', $type)
-                            ->where('part_dock', $dock)
-                            ->first(); // dev-1.0, Ferry, 20170908, set to first //dev-1.0, by yudo, 20170609, change part number customer
-                            
-        try{    
+        $part_pis   = avi_part_pis::whereRaw('REPLACE(part_number_customer, "-", "") LIKE "%' . $image . '%"')
+            ->where('part_kind', $type)
+            ->where('part_dock', $dock)
+            ->first(); // dev-1.0, Ferry, 20170908, set to first //dev-1.0, by yudo, 20170609, change part number customer
 
-            if(! $part_pis)
-            {       
+        try {
+
+            if (!$part_pis) {
                 // return response()->json($part);      // dev-1.0, Ferry, Commented ganti yg lebih bersih
                 return array("part_number_customer" => "");
-            }
-            else{
+            } else {
 
                 DB::beginTransaction();
 
@@ -98,113 +97,114 @@ class PisController extends Controller
                 DB::commit();
 
                 $counter = avi_mutations::where('mutation_date', date('Y-m-d'))
-                                        ->where('mutation_code', config('avi_mutation.gi_out_delivery'))
-                                        ->where('npk', $user->npk)
-                                        ->count();
+                    ->where('mutation_code', config('avi_mutation.gi_out_delivery'))
+                    ->where('npk', $user->npk)
+                    ->count();
 
                 // dev-1.0, Ferry, 20170926, ambil info last scan
                 $last_scan = avi_mutations::selectRaw('back_number, part_kind, COUNT(id) AS total_kanban')
-                                            ->where('mutation_code', config('avi_mutation.gi_out_delivery'))
-                                            ->where('npk', $user->npk)
-                                            ->groupBy('back_number', 'part_kind')
-                                            ->orderBy('created_at', 'desc')
-                                            ->take(5)
-                                            ->get();
+                    ->where('mutation_code', config('avi_mutation.gi_out_delivery'))
+                    ->where('npk', $user->npk)
+                    ->groupBy('back_number', 'part_kind')
+                    ->orderBy('created_at', 'desc')
+                    ->take(5)
+                    ->get();
 
                 // return response()->json($part);      // dev-1.0, Ferry, Commented ganti yg lebih bersih
                 $arrJSON = array(
-                                "img_path" => Storage::exists('/public/pis/'.$part_pis->part_number_customer.$path_suffix) ? 
-                                                asset('storage/pis/'.$part_pis->part_number_customer.$path_suffix) :
-                                                asset('storage/pis/default.JPG'),
-                                "part_number_customer" => $part_pis->part_number_customer,
-                                "counter"   => $counter,
-                                "last_scan" => $last_scan
-                        );
+                    "img_path" => Storage::exists('/public/pis/' . $part_pis->part_number_customer . $path_suffix) ?
+                        asset('storage/pis/' . $part_pis->part_number_customer . $path_suffix) :
+                        asset('storage/pis/default.JPG'),
+                    "part_number_customer" => $part_pis->part_number_customer,
+                    "counter"   => $counter,
+                    "last_scan" => $last_scan
+                );
 
                 return $arrJSON;
             }
-
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
 
             DB::rollBack();
-            return array( "part_number_customer" => "", "error" => $e->getMessage() );
+            return array("part_number_customer" => "", "error" => $e->getMessage());
         }
-        
-
     }
-    
 
-    function viewDashboardMutation(){
+
+    function viewDashboardMutation()
+    {
 
         return view('adminlte::dashboard.mutation');
-    } 
+    }
 
 
-    function getAjaxMutation(){ 
+    function getAjaxMutation()
+    {
 
         $arr_result = avi_mutations::selectRaw('SUM(quantity) AS new_qty, part_number')
-                                                    ->where('store_location', config('global.warehouse_body.finish_good'))
-                                                    ->groupBy('part_number')
-                                                    ->get();
+            ->where('store_location', config('global.warehouse_body.finish_good'))
+            ->groupBy('part_number')
+            ->get();
         return $arr_result;
-    } 
+    }
 
-    function PisMasterView(){
-       //dev-1.0, ambil data pis dari avi_part_pis
+    function PisMasterView()
+    {
+        //dev-1.0, ambil data pis dari avi_part_pis
         $avi_part_piss  = avi_part_pis::selectRaw("0 as validasi, id, part_number_customer, part_number, back_number, qty_kanban, part_kind, part_dock, CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path")
-                                        ->get();    
-                                        // return $avi_part_piss;
+            ->get();
+        // return $avi_part_piss;
         foreach ($avi_part_piss as $avi_part_pis) {
-                                        $avi_part_pis->validasi = Storage::exists('/public/pis/'.$avi_part_pis->img_path) ? "Ada" : "Belum Ada"; 
-                                        }                                
-                                   // return $avi_part_piss;
-        return view('pis.ViewMasterPis',compact('avi_part_piss'));
+            $avi_part_pis->validasi = Storage::exists('/public/pis/' . $avi_part_pis->img_path) ? "Ada" : "Belum Ada";
+        }
+        // return $avi_part_piss;
+        return view('pis.ViewMasterPis', compact('avi_part_piss'));
     }
 
-    function UpdatePis($id){
-       //dev-1.0, ambil data pis dari avi_part_pis untuk tampil di view update
-        $avi_part_piss  = avi_part_pis::select("*",DB::raw("CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
-                                        ->where('id',$id)
-                                        ->get();
-                                        
-        return view('pis.UpdatePis',compact('avi_part_piss'));
+    function UpdatePis($id)
+    {
+        //dev-1.0, ambil data pis dari avi_part_pis untuk tampil di view update
+        $avi_part_piss  = avi_part_pis::select("*", DB::raw("CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
+            ->where('id', $id)
+            ->get();
+
+        return view('pis.UpdatePis', compact('avi_part_piss'));
     }
 
-    function UpdatePisProses(Request $request){
-       
-        $input              =\Request::all();
+    function UpdatePisProses(Request $request)
+    {
 
-        $id                 =$input['id'];
-        $part_number        =$input['part_number'];
-        $back_no            =$input['back_no']; 
-        $dock               =$input['part_dock'];
-        $type               =$input['type']; 
-        $qty                =$input['qty'];
-        $img_path           =$input['part_picture'];
-        $destinationPath    =asset('storage/pis');
+        $input              = \Request::all();
 
-        try{
+        $id                 = $input['id'];
+        $part_number        = $input['part_number'];
+        $back_no            = $input['back_no'];
+        $dock               = $input['part_dock'];
+        $type               = $input['type'];
+        $qty                = $input['qty'];
+        $img_path           = $input['part_picture'];
+        $destinationPath    = asset('storage/pis');
+
+        try {
             \DB::beginTransaction();
 
             // simpan ke database
-            $avp=avi_part_pis::find($id);
-            $avp->part_number_customer=$part_number;
-            $avp->back_number=$back_no;
-            $avp->qty_kanban=$qty;
-            $avp->part_dock=$dock;
-            $avp->part_kind=$type;
-            $avp->save(); 
+            $avp = avi_part_pis::find($id);
+            $avp->part_number_customer = $part_number;
+            $avp->back_number = $back_no;
+            $avp->qty_kanban = $qty;
+            $avp->part_dock = $dock;
+            $avp->part_kind = $type;
+            $avp->save();
 
             //upload gambar ke 
             $file = $request->file('part_picture');
-            $filesName = $part_number.'-'.$type.'-'.$dock.'.JPG';
-            if (Storage::exists('/public/pis/'.$filesName)) {
+            $filesName = $part_number . '-' . $type . '-' . $dock . '.JPG';
+            if (Storage::exists('/public/pis/' . $filesName)) {
                 $delete = Storage::disk('pis')->delete($filesName);
             }
             // return $file ;
             // $temp = Storage::put('pis/'.$filesName , $file);
-            $temp = Storage::disk('pis') -> put($filesName, file_get_contents($file->getRealPath()));
+            $temp = Storage::disk('pis')->put($filesName, file_get_contents($file->getRealPath()));
             // $temp = Storage::move($file, asset('storage/pis/'.$filesName));
             // return $temp ;
 
@@ -212,120 +212,121 @@ class PisController extends Controller
             \DB::commit();
             \Session::flash('flash_type', 'alert-success');
             \Session::flash('flash_message', 'Sukses simpan atur ulang data');
-            
+
             return redirect('/pis/master');
-        }catch(Exception $e){
+        } catch (Exception $e) {
             \DB::rollback();
             \Session::flash('flash_type', 'alert-danger');
-            \Session::flash('flash_message', 'Gagal mengatur ulang data karena '.$e->getMessage());
+            \Session::flash('flash_message', 'Gagal mengatur ulang data karena ' . $e->getMessage());
             return \Redirect::Back();
         }
-                                       
+
         return url('/pis/master');
     }
 
-    function PisPreview($img){
-       
-       $arrJSONpict = array(
-                        
-        $img_path = Storage::exists('/public/pis/'.$img) ? 
-                      asset('storage/pis/'.$img) :
-                      asset('storage/pis/default.JPG')
-                    );
-       
-       return view('pis.PisPreview',compact('img_path'));
+    function PisPreview($img)
+    {
+
+        $arrJSONpict = array(
+
+            $img_path = Storage::exists('/public/pis/' . $img) ?
+                asset('storage/pis/' . $img) :
+                asset('storage/pis/default.JPG')
+        );
+
+        return view('pis.PisPreview', compact('img_path'));
     }
 
-    function PisSearch(){
-        $input=\Request::all();
-       
-        $oem=$input['oem'];
+    function PisSearch()
+    {
+        $input = \Request::all();
 
-        $gnp=$input['gnp'];
-        $dock_4N=$input['dock_4N']; 
-        $dock_4L=$input['dock_4L'];
+        $oem = $input['oem'];
 
-            if(isset($input['oem']) or isset($input['gnp'])){ 
-                $avi_part_piss  = avi_part_pis::select("*",DB::raw("CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
-                                        ->whereIn('part_kind',[$oem,$gnp])
-                                        ->orWhereIn('part_dock',[$dock_4L,$dock_4N])
-                                        ->get();
-                
-            }else{
+        $gnp = $input['gnp'];
+        $dock_4N = $input['dock_4N'];
+        $dock_4L = $input['dock_4L'];
 
-            }
+        if (isset($input['oem']) or isset($input['gnp'])) {
+            $avi_part_piss  = avi_part_pis::select("*", DB::raw("CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
+                ->whereIn('part_kind', [$oem, $gnp])
+                ->orWhereIn('part_dock', [$dock_4L, $dock_4N])
+                ->get();
+        } else {
+        }
 
-            if(isset($input['dock_4N']) or isset($input['dock_4L'])){ //oem dan gnp
-                $avi_part_piss  = avi_part_pis::select("*",DB::raw("CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
-                                        ->whereIn('part_kind',[$oem,$gnp])
-                                        ->orWhereIn('part_dock',[$dock_4L,$dock_4N])
-                                        ->get();
-            }else{
-
-            }
+        if (isset($input['dock_4N']) or isset($input['dock_4L'])) { //oem dan gnp
+            $avi_part_piss  = avi_part_pis::select("*", DB::raw("CONCAT(part_number_customer,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
+                ->whereIn('part_kind', [$oem, $gnp])
+                ->orWhereIn('part_dock', [$dock_4L, $dock_4N])
+                ->get();
+        } else {
+        }
 
         // $avi_part_piss  = avi_part_pis::select("*",DB::raw("CONCAT(part_number,'-',part_kind,'-',part_dock,'.JPG') as img_path"))
         //                                 ->whereIn('part_kind',[$oem,$gnp])
         //                                 ->orWhereIn('part_dock',[$dock_4L,$dock_4N])
         //                                 ->get();
-                                        // return $avi_part_piss;       
-        return view('pis.ViewMasterPis',compact('avi_part_piss'));
+        // return $avi_part_piss;       
+        return view('pis.ViewMasterPis', compact('avi_part_piss'));
     }
 
-    function GetAjaxPartPis(){  // dev-1.0 ,Handika, 20171019, get data part no in pis form 
-        $term=\Request::all();
-        if(!isset($term['q'])){
+    function GetAjaxPartPis()
+    {  // dev-1.0 ,Handika, 20171019, get data part no in pis form 
+        $term = \Request::all();
+        if (!isset($term['q'])) {
             return [];
         }
-        $term=$term['q'];
-        if(strlen($term) < 2){
+        $term = $term['q'];
+        if (strlen($term) < 2) {
             return [];
         }
-        return avi_parts::where('part_number','like','%'.$term.'%')
+        return avi_parts::where('part_number', 'like', '%' . $term . '%')
             ->get()->toArray();
     }
 
-    function validasi(){ // dev-1.0 ,Fahrul, 20171031, validasi form with ajax
+    function validasi()
+    { // dev-1.0 ,Fahrul, 20171031, validasi form with ajax
 
         $input                  = Input::all();
         $part_number            = $input['part_number_aiia'];
-        $part_number_customer   = $input['part_number_customer'];      
+        $part_number_customer   = $input['part_number_customer'];
         $part_kind              = $input['part_kind'];
         $part_dock              = $input['part_dock'];
         $part_name              = $input['part_name'];
         $min_stock              = $input['min_stock'];
         $part_no                = $input['part_number'];
-      
-    
+
+
         if ($min_stock = is_null($min_stock)) { //cek apakah manual input atau tidak
-            $jumlah = avi_part_pis::where('part_number_customer',$part_number_customer) //mengambil data dari avi_part_pis
-            ->where('part_kind',$part_kind)
-            ->where('part_dock',$part_dock)
-            ->count();
+            $jumlah = avi_part_pis::where('part_number_customer', $part_number_customer) //mengambil data dari avi_part_pis
+                ->where('part_kind', $part_kind)
+                ->where('part_dock', $part_dock)
+                ->count();
 
             if ($part_no = is_null($part_no)) {
                 return "error";
-            }
-            elseif ($jumlah == 0) {
+            } elseif ($jumlah == 0) {
 
                 return "save";
-            }else{
+            } else {
                 return "ada";
             } //berhasil
 
-            
-        }else{
-            $jumlah = avi_parts::where('part_number',$part_number) //mengambil data dari avi_parts
-            ->count();
+
+        } else {
+            $jumlah = avi_parts::where('part_number', $part_number) //mengambil data dari avi_parts
+                ->count();
 
             if ($jumlah == 0) {
                 return "save1";
-            }else{
+            } else {
                 return "ada";
             }
         }
     }
-    function addpis(Request $request){ //fungsi bukan manual input 
+    function addpis(Request $request)
+    { //fungsi bukan manual input 
 
         $input                  = \Request::all();
         $part_number_customer   = $input['part_number_customer'];
@@ -354,7 +355,8 @@ class PisController extends Controller
 
         return redirect('/pis/master');
     }
-    function addpart(){ //fungsi manual input
+    function addpart()
+    { //fungsi manual input
 
         $input                  = \Input::all();
         $part_number_aiia       = $input['part_number_aiia'];
@@ -362,11 +364,11 @@ class PisController extends Controller
         $part_number_customer   = $input['part_number_customer'];
         $back_number            = $input['back_number'];
         $min_stock              = $input['min_stock'];
-        $max_stock              = $input['max_stock'];        
+        $max_stock              = $input['max_stock'];
         $part_kind              = $input['part_kind'];
         $qty_kanban             = $input['qty_kanban'];
         $part_dock              = $input['part_dock'];
-        $part_number_nostrip    = str_replace("-","", $part_number_aiia); //mengganti value tanpa strip
+        $part_number_nostrip    = str_replace("-", "", $part_number_aiia); //mengganti value tanpa strip
 
 
         $pis                        = new avi_part_pis;
